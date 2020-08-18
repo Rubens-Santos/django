f00165e19ecd0b1813d9a0e7435326062e39c4b4
Projeto Rubens <rubens.s.augusto@gmail.com>
2020-08-18 16:15:53 -0300

    banco de dados

--- fusion/settings.py
+++ fusion/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -91,7 +91,7 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config()
 }
-"""
+
 
 
 # Password validation
